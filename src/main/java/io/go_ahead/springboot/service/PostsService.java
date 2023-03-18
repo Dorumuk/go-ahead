05011fe859de5fbb44037fca84de4765a7e8b134
Dorumuk <haulinef@gmail.com>
2023-03-18 19:56:43 +0900

    Learn Chapter4
    -글 수정/삭제 기능 추가

--- src/main/java/io/go_ahead/springboot/service/PostsService.java
+++ src/main/java/io/go_ahead/springboot/service/PostsService.java
@@ -2,12 +2,15 @@ package io.go_ahead.springboot.service;
 
 import io.go_ahead.springboot.domain.posts.Posts;
 import io.go_ahead.springboot.domain.posts.PostsRepository;
+import io.go_ahead.springboot.web.dto.PostsListResponseDto;
 import io.go_ahead.springboot.web.dto.PostsResponseDto;
 import io.go_ahead.springboot.web.dto.PostsSaveRequestDto;
 import io.go_ahead.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -23,17 +26,33 @@ public class PostsService {
     @Transactional // 💥❓
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // == .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/io/go_ahead/springboot/web/IndexController.java
+++ src/main/java/io/go_ahead/springboot/web/IndexController.java
@@ -1,24 +1,39 @@
 package io.go_ahead.springboot.web;
 
+import io.go_ahead.springboot.service.PostsService;
+import io.go_ahead.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
 
     /*
      * src/main/resources/templates 로 앞의 경로가 고정됨.
      * mustache starter 라이브러리로 인해 .mustache 확장자로 자동 지정
      * */
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
